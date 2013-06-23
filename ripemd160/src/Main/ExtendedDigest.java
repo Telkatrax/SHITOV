139e1572d259b93c7cd105c93cc1ee6dbdc649e7
Artem <Telkatrax@yandex.ru>
2013-06-23 17:06:01 +0800

    ripemd160

---
+++ ripemd160/src/Main/ExtendedDigest.java
@@ -0,0 +1,7 @@
+package Main;
+public interface ExtendedDigest 
+    extends Digest
+{
+
+    public int getByteLength();
+}
---
+++ ripemd160/src/Main/InterfaceError.java
@@ -0,0 +1,21 @@
+package Main;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class InterfaceError  {
+
+	private String textError = null;
+	public InterfaceError(String textError) {
+		
+		this.textError=textError;
+		JOptionPane errorPane = new JOptionPane(this.textError, JOptionPane.QUESTION_MESSAGE);
+		JDialog dialog = errorPane.createDialog(new JFrame(), "Manual Creation");
+	    dialog.setLocationRelativeTo(null);
+		dialog.setTitle("Œÿ»¡ ¿");
+		dialog.setVisible(true);
+		
+	}
+
+}
